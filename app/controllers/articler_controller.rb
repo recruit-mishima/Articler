87a75d1aaa03dfdfd666d28e5f9a2671f2bc9b73
MishimaKenji <kenji.mishima91@icloud.com>
2017-01-02 15:31:51 +0900

    still working on

--- app/controllers/articler_controller.rb
+++ app/controllers/articler_controller.rb
@@ -9,6 +9,12 @@ class ArticlerController < ApplicationController
   end
   def index
    # @articles = Article.find_by_sql(["select * from articles where title like ?", params[:title]])
-   @articles = Article.where('title LIKE(?)', "%#{params[:title]}%")
+   @articles = Article.where('title LIKE(?)', "%#{params[:title]}%").order("date DESC")
   end
+  def show
+    binding.pry
+    @articler = Article.find(params[:id])
+  end
+
+
 end
